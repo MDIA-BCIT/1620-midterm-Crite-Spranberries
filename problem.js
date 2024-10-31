84b02485b9b4bb047f1d0a3dd3e3862e61bfd4dd
Crite-Spranberries <113459843+Crite-Spranberries@users.noreply.github.com>
2024-10-31 14:58:38 -0700

    Added checkPassword function along with all necessary variables, comments. conditionals,  and calls. Yippee.

--- problem.js
+++ problem.js
@@ -13,3 +13,51 @@ CHALLENGE
 - If the password is less than 5 characters, log out "Your password is too short!".
 */
 
+
+
+function checkPassword(password, userInput){
+    
+    let flagForOptimal;                                                                             // Checks if the passwords are "forgot" or "reset" to account for telling the user the password name is not ideal
+    if ((password == "forgot") || (password =="reset")) {
+        flagForOptimal = false;
+    } else flagForOptimal = true;
+
+
+    if (password.length < 5){                                                                       // Checks if password is too short
+        console.log("Your password is too short!");
+    } else if ((flagForOptimal == false) && (userInput == password)){                               // Checks if input is valid + if its labelled as "reset" or "forgot"
+        console.log("Access Granted!");
+        console.log("This password you set should not be used because it glitches the system");
+    } else if (userInput == password){                                                              // Checks if password is valid
+        console.log("Access Granted!");
+    } else if (userInput == "forgot"){                                                              // Provides user with hint if user inputs "forgot"
+        console.log(`Here is a hint! The password starts with: "${password.charAt(0)}".`);             
+    } else if (userInput == "reset"){                                                               // Prompts user with text if user inputs "reset"
+        console.log("Let's reset your account")
+    }   else if (userInput != password){                                                            // Checks if input is invalid
+        console.log("Access Denied!");
+    }
+}
+
+
+// checkPassword([password], [input]);
+
+checkPassword("potatoes", "potatoes");
+
+
+
+
+// // Sample password variable bank I used for preset word testing
+// let chosenWord0 = "reset";                  //To establish "reset" as password
+// let chosenWord1 = "forgot";                 //To establish "forgot" as password
+// let chosenWord2 = "xXxcodem4ster1010xXx";   //To establish the "correct" password
+// let chosenWord3 = "smol";                   //To establish small invalid password(>5) size
+
+// // Input variable. Change this accordingly! 
+// let input = "xXxcodem4ster1010xXx";
+
+// // Password variable. Change this accordingly! 
+// let setPassword = chosenWord2;
+
+// // Calls the function. These are preset variables that can be swapped above, but typing in
+// // straight strings also works!
